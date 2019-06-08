6111b0e2e3c83e7dec548e1926708d21841769d0
Travis Williamson <cthwgw1791@gmail.com>
2019-06-07 18:47:56 -0700

    Started working on map

---
+++ 8Kyu_LostWithoutAMap.js
@@ -0,0 +1,21 @@
+/***********************************/
+// 8kyu Lost Without A Map
+/***********************************/
+
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+
+// Input: An array of integers
+// Output: A new array with each value doubled
+// Edge cases:
+// Restrictions: 
+
+const doubledUp = (x) => {
+    map1 = 
+}
+console.log(doubledUp([1,2,3]))
